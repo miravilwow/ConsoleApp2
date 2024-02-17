c72cda0d5f1d749a10138dbbbfc643ac0ccab1d5
ENDUSER <ENDUSER@DESKTOP-9I2TKNK>
2024-02-17 19:49:12 +0800

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name :");
+            String name = Console.ReadLine();
+
+            Console.WriteLine("Enter the total number of your enrolled courses : ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the price of your favorite book :");
+            double price = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Name " + name);
+            Console.WriteLine("Total enrolled courses : " + number);
+            Console.WriteLine("Price of my favorite book : " + price);
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
